6cc4360a8007d670e44b03786c148a889451954d
niukunlong <niukunlong4379@ienglish.cn>
2024-05-17 09:32:29 +0800

    新增

---
+++ index.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: niukunlong niukunlong4379@ienglish.cn
+ * @Date: 2024-04-28 13:30:58
+ * @LastEditors: niukunlong niukunlong4379@ienglish.cn
+ * @LastEditTime: 2024-04-28 13:31:12
+ * @FilePath: \tope-message-center-frontc:\Users\TPLN\Desktop\top-coverage-webpack-plugin\index.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+module.exports = require("./lib/plugin");
